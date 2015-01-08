cb77a0c0b31e570296450baccb431993bc7d9bb4
Long Yang <598251812@qq.com>
2015-01-08 11:06:31 +0800

    megar web_estate

---
+++ web_estate/response.ashx
@@ -0,0 +1,55 @@
+﻿<%@ WebHandler Language="C#" Class="response" %>
+
+using System;
+using System.Web; 
+using System.Web.Services;
+using System.Collections.Generic;
+using System.Reflection;
+using MongoDB.Bson;
+
+public class response : IHttpHandler {
+    
+    public void ProcessRequest (HttpContext context) {
+
+        BsonDocument doc = new BsonDocument();
+        if (context.Request.QueryString["class"] != null && context.Request.QueryString["method"] != null && context.Request.QueryString["input"] != null)
+        {
+
+            string str_class = context.Request.QueryString["class"].ToString();
+            string str_method = context.Request.QueryString["method"].ToString();
+            string str_input = context.Request.QueryString["param"].ToString();
+
+            try
+            {
+                Type reflect_type = Assembly.Load("App_Code").GetType(str_class);
+                object reflect_acvtive = Activator.CreateInstance(reflect_type, null);
+
+                MethodInfo method_info = reflect_type.GetMethod(str_method);
+                string result = (string)method_info.Invoke(reflect_acvtive, new object[] { str_input });
+
+                context.Response.Write(result);
+            }
+            catch (Exception)
+            {
+                doc.Add(new BsonElement("state", "0"));
+                doc.Add(new BsonElement("info", "class or method or prama is wrong!"));
+                context.Response.Write(doc.ToString());
+            }
+
+        }
+        else
+        {
+            doc.Add(new BsonElement("state", "0"));
+            doc.Add(new BsonElement("info", "class or method or param is empty!"));
+            context.Response.Write(doc.ToString());
+        }
+       
+    }
+ 
+    public bool IsReusable {
+        get {
+            return false;
+        }
+    }
+
+}
---
+++ web_estate/response.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq; 
+using System.Web.Services;
+using System.Collections.Generic;
+using System.Reflection;
+using MongoDB.Bson;
+public partial class response : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {  
+
+        BsonDocument doc = new BsonDocument();
+        if (Request.QueryString["type"] != null && Request.QueryString["method"] != null && Request.QueryString["input"] != null)
+        {
+
+            string str_class = Request.QueryString["type"].ToString();
+            string str_method = Request.QueryString["method"].ToString();
+            string str_input = Request.QueryString["input"].ToString();
+
+
+            try
+            {
+                Type reflect_type = Assembly.Load("App_Code").GetType(str_class);
+                object reflect_acvtive = Activator.CreateInstance(reflect_type, null);
+
+                MethodInfo method_info = reflect_type.GetMethod(str_method);
+                string result = (string)method_info.Invoke(reflect_acvtive, new object[] { str_input });
+
+                Response.Write(result);
+            }
+            catch (Exception error)
+            {
+                doc.Add(new BsonElement("state", "0"));
+                doc.Add(new BsonElement("info", error.Message));
+                Response.Write(doc.ToString());
+            }
+
+        }
+        else
+        {
+            doc.Add(new BsonElement("state", "0"));
+            doc.Add(new BsonElement("info", "class or method or parameter is empty!"));
+            Response.Write(doc.ToString());
+        }
+       
+    }
+}
+
+
